1c362b3cec6984b2779d73adbf6a3011f6c5ea54
J.Fabiola Juárez Z <jefajuza@hotmail.com>
2019-07-19 00:51:11 -0500

    ya esta implementado el autentificarse con google

--- src/app/providers/login.service.ts
+++ src/app/providers/login.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
+
+  login(proveedor: string) {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
